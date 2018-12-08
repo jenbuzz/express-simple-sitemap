6f699eb77fa0f49794d49c977e9874c3ad9a8f8a
dan-lyn <daniel_lynge@hotmail.com>
2018-12-08 21:52:43 +0100

    added typescript definition

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+export interface SitemapUrl {
+    url: string;
+    lastmod?: string;
+    changefreq?: string;
+    priority?: number;
+}
+
+export interface SitemapOptions {
+    sitemapUrl?: string;
+    urls?: SitemapUrl[];
+}
+
+/**
+ * Send a response with a sitemap containing specified URLs
+ * if specified sitemap url is requested.
+ * 
+ * @param options containing sitemapUrl and urls.
+ * @returns express middleware function.
+ */
+export default function sitemap(options?: SitemapOptions): (req, res, next) => void;
