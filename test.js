c82794c397148d9b8c6b2379d37a0a254603ca18
dan-lyn <daniel_lynge@hotmail.com>
2018-12-11 09:30:50 +0100

    added test for partly filled url objects

--- test.js
+++ test.js
@@ -58,3 +58,72 @@ test('get filled sitemap from request', () => {
 </urlset>`
     );
 });
+
+test('get filled sitemap with partly url objects from request', () => {
+    const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/sitemap.xml',
+    });
+
+    const response = httpMocks.createResponse();
+
+    sitemap({
+        urls: [
+            {
+                url: 'https://github.com/jenbuzz',
+            },
+            {
+                url: 'https://github.com/jenbuzz',
+                lastmod: '2018-12-08',
+            },
+            {
+                url: 'https://github.com/jenbuzz',
+                lastmod: '2018-12-08',
+                changefreq: 'daily',
+            },
+            {
+                url: 'https://github.com/jenbuzz',
+                lastmod: '2018-12-08',
+                changefreq: 'daily',
+                priority: 0,
+            },
+        ],
+    })(request, response, () => {});
+
+    expect(response.getHeader('Content-Type')).toEqual('application/xml');
+
+    expect(response._getData()).toBe(
+`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+    
+<url>
+    <loc>https://github.com/jenbuzz</loc>
+    
+    
+    
+</url>
+    
+<url>
+    <loc>https://github.com/jenbuzz</loc>
+    <lastmod>2018-12-08</lastmod>
+    
+    
+</url>
+    
+<url>
+    <loc>https://github.com/jenbuzz</loc>
+    <lastmod>2018-12-08</lastmod>
+    <changefreq>daily</changefreq>
+    
+</url>
+    
+<url>
+    <loc>https://github.com/jenbuzz</loc>
+    <lastmod>2018-12-08</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0</priority>
+</url>
+    
+</urlset>`
+    );
+});
