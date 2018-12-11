dafb3bd900f0861609245fe4553a7bcb1b2aa490
dan-lyn <daniel_lynge@hotmail.com>
2018-12-11 09:04:34 +0100

    added test for reponse with data

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ test('add sitemap middleware to express application', () => {
     app.use(sitemap());
 });
 
-test('get sitemap from request', () => {
+test('get empty sitemap from request', () => {
     const request = httpMocks.createRequest({
         method: 'GET',
         url: '/sitemap.xml',
@@ -20,4 +20,38 @@ test('get sitemap from request', () => {
     sitemap()(request, response, () => {});
 
     expect(response.getHeader('Content-Type')).toEqual('application/xml');
+});
+
+test('get filled sitemap from request', () => {
+    const request = httpMocks.createRequest({
+        method: 'GET',
+        url: '/site-map.xml',
+    });
+
+    const response = httpMocks.createResponse();
+
+    sitemap({
+        sitemapUrl: '/site-map.xml',
+        urls: [
+            {
+                url: 'https://github.com/jenbuzz',
+                lastmod: '2018-12-08',
+                changefreq: 'daily',
+                priority: 1.0,
+            },
+        ],
+    })(request, response, () => {});
+
+    expect(response.getHeader('Content-Type')).toEqual('application/xml');
+
+    expect(response._getData()).toBe(`<?xml version="1.0" encoding="UTF-8"?>
+                <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+                    <url>
+                        <loc>https://github.com/jenbuzz</loc>
+                        <lastmod>2018-12-08</lastmod>
+                        <changefreq>daily</changefreq>
+                        <priority>1</priority>
+                    </url>
+                </urlset>`
+    );
 });
